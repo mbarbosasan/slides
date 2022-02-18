f6493450cacf0ad6e231bdd94757259f02f0a4e0
moisesbarbosa23@gmail.com <moisesbarbosa23@gmail.com>
2022-02-18 13:06:25 -0300

    Ajuste das configurações ao resize e classe ativa adicionada com debounce

--- js/slide.js
+++ js/slide.js
@@ -1,3 +1,5 @@
+import debounce from './debounce.js';
+
 export default class Slide {
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide);
@@ -7,6 +9,8 @@ export default class Slide {
       startX: 0,
       movement: 0,
     }
+
+    this.activeClass = 'active';
   }
 
   transition(active) {
@@ -68,13 +72,6 @@ export default class Slide {
     this.wrapper.addEventListener('touchend', this.onEnd)
 
   }
-
-  bindEvents() {
-    this.onStart = this.onStart.bind(this);
-    this.onMove = this.onMove.bind(this);
-    this.onEnd = this.onEnd.bind(this);
-  }
-
   //Slides config
 
   slidePosition(slide) {
@@ -106,7 +103,15 @@ export default class Slide {
     this.moveSlide(activeSlide.position)
     this.slidesIndexNav(index);
     this.dist.finalPosition = activeSlide.position;
+    this.changeActiveClass();
   }
+
+  changeActiveClass() {
+    this.slideArray.forEach((item) => {
+      item.element.classList.remove(this.activeClass);
+    })
+    this.slideArray[this.index.actual].element.classList.add(this.activeClass)
+    }
 
   activePrevSlide() {
     if (this.index.previous !== undefined) {
@@ -120,11 +125,31 @@ export default class Slide {
     }
   }
 
+  
+  onResize() {
+    setTimeout(() => {
+      this.slidesConfig();
+      this.changeSlide(this.index.actual);
+    }, 1000)
+  }
+  
+  addResizeEvent() {
+    window.addEventListener('resize', this.onResize);
+  }
+
+  bindEvents() {
+    this.onStart = this.onStart.bind(this);
+    this.onMove = this.onMove.bind(this);
+    this.onEnd = this.onEnd.bind(this);
+    this.onResize = debounce(this.onResize.bind(this), 200);
+  }
+
   init() {
     this.bindEvents();
     this.transition(true);
     this.slidesConfig();
     this.addSlideEvents();
+    this.addResizeEvent();
     return this;
   }
 }
